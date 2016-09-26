e546ea3704e4a9721ff00e8d97326978ef73e2d7
Ninjaprawn <george.r.dan@gmail.com>
2016-09-26 10:30:31 +1000

    Added trending repos

--- example.js
+++ example.js
@@ -1,9 +1,18 @@
 var exp = require('./index') || require('explore-api');
 
-exp.topShowcases(function(showcases) {
-	exp.showcaseDetails(showcases[0].url, function(showcase) {
-		console.log(showcases[0]);
-		console.log("---------------------");
-		console.log(showcase);
-	});
-});
+// exp.topShowcases(function(showcases) {
+// 	console.log(showcases[0]);
+// 	exp.showcaseDetails(showcases[0].url, function(showcase) {
+// 		console.log(showcase);
+// 	});
+// });
+
+
+
+// exp.availableTrendingLanguages(function(langs) {
+// 	console.log(langs);
+// });
+
+exp.trendingRepos(function(repos) {
+	console.log(repos);
+}, "", "monthly");
--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 var cheerio = require("cheerio");
 var request = require("request");
 
+// Function from http://stackoverflow.com/a/24941988
+function isString(obj) {
+  return (Object.prototype.toString.call(obj) === '[object String]');
+}
+
 var api = {};
 
 api.topShowcases = function(callback) {
@@ -12,27 +17,32 @@ api.topShowcases = function(callback) {
 
 				var showcase = {};
 
-				showcase.url = "https://github.com" + $(this).attr('href');
+				showcase.url = "https://github.com" + $(this).find('a').attr('href');
 				showcase.title = $(this).find("h3").text().trim();
-				showcase.description = $(this).text().replace($(this).find("h3").text(), '').replace($($(this).children()[1]).next().text(), '').trim();
-				showcase.repositories = parseInt($($($(this).children()[1]).next().children()[0]).text().trim().split(" ")[0]);
-				showcase.languages = parseInt($($($(this).children()[1]).next().children()[1]).text().trim().split(" ")[0]);
+				showcase.description = $(this).find("p").text();
+				showcase.repositories = parseInt($($($(this).find("a").children()[3]).children()[0]).text().trim().split(" ")[0]);
+				showcase.languages = parseInt($($($(this).find("a").children()[3]).children()[1]).text().trim().split(" ")[0]);
 
 				showcases.push(showcase);
 
 			});
 			if (callback) {
 				callback(showcases);
-			}
+			} else {
+                throw "Callback required";
+            }
         } else if (err && resp.statusCode != 200) {
-            console.log("Error: " + err + "\n with status code: " + resp.statusCode);
+            throw "Error: " + err + "\n with status code: " + resp.statusCode;
         } else {
-            console.log("Unknown error");
+            throw "Unknown error";
         }
     });
 };
 
 api.showcaseDetails = function(showcaseURL, callback) {
+    if (!isString(showcaseURL)) {
+        throw "showcaseURL must be of type 'String'";
+    }
     request(showcaseURL, function(err, resp, html) {
         if (!err && resp.statusCode == 200) {
             var $ = cheerio.load(html);
@@ -42,6 +52,50 @@ api.showcaseDetails = function(showcaseURL, callback) {
             showcase.description = $($(".showcase-page-header").children()[0]).find("p").text().trim();
             showcase.languages = $($($($(".showcase-page-header").children()[0]).children()[2]).children()[1]).attr("aria-label").split(", ");
             showcase.languages[showcase.languages.length-1] = showcase.languages[showcase.languages.length-1].replace("and ", "").replace(".", "");
+
+            showcase.repositories = [];
+            $(".repo-list").children().each(function(i, element) {
+                var repository = {};
+                repository.author = $(this).find("h3").text().split("/")[0].trim();
+                repository.name = $(this).find("h3").text().split("/")[1].trim();
+                repository.description = $(this).find(".repo-list-description").text().trim();
+                repository.url = "https://github.com/" + repository.author + "/" + repository.name;
+                repository.image = $(this).find("img").attr("src");
+
+                repository.stars = parseInt($($($(this).find(".repo-list-stats")).children()[0]).text().split(",").join("").trim());
+                repository.forks = parseInt($($($(this).find(".repo-list-stats")).children()[1]).text().split(",").join("").trim());
+                repository.language = $($(this).find(".repo-list-stats")).text().replace($($($(this).find(".repo-list-stats")).children()[0]).text(), "").replace($($($(this).find(".repo-list-stats")).children()[1]).text(), "").trim();
+
+                showcase.repositories.push(repository);
+            });
+
+			if (callback) {
+				callback(showcase);
+			} else {
+                throw "Callback required";
+            }
+        } else if (err && resp && resp.statusCode != 200) {
+            throw "Error: " + err + "\n with status code: " + resp.statusCode;
+        } else {
+            throw "Unknown error"
+        }
+    });
+}
+
+/*api.allShowcases = function(callback, maxNumberOfShowcases, start) {
+    if !parseInt(maxNumberOfShowcases) || !parseInt(start) {
+        throw "Must parse integers in <maxNumberOfShowcases> and <start>";
+    }
+    request(showcaseURL, function(err, resp, html) {
+        if (!err && resp.statusCode == 200) {
+            var $ = cheerio.load(html);
+			var showcase = {};
+
+            showcase.title = $($(".showcase-page-header").children()[0]).find("h1").text().trim();
+            showcase.description = $($(".showcase-page-header").children()[0]).find("p").text().trim();
+            showcase.languages = $($($($(".showcase-page-header").children()[0]).children()[2]).children()[1]).attr("aria-label").split(", ");
+            showcase.languages[showcase.languages.length-1] = showcase.languages[showcase.languages.length-1].replace("and ", "").replace(".", "");
+
             showcase.repositories = [];
             $(".repo-list").children().each(function(i, element) {
                 var repository = {};
@@ -60,11 +114,83 @@ api.showcaseDetails = function(showcaseURL, callback) {
 
 			if (callback) {
 				callback(showcase);
-			}
+			} else {
+                throw "Callback required";
+            }
         } else if (err && resp.statusCode != 200) {
-            console.log("Error: " + err + "\n with status code: " + resp.statusCode);
+            throw "Error: " + err + "\n with status code: " + resp.statusCode;
         } else {
-            console.log("Unknown error");
+            throw "Unknown error"
+        }
+    });
+}*/
+
+api.availableTrendingLanguages = function(callback) {
+    if (!callback) {
+        throw "Callback required";
+    }
+    request("https://github.com/trending", function(err, resp, html) {
+        if (!err && resp.statusCode == 200) {
+            var $ = cheerio.load(html);
+
+            var languages = [{name:"All languages", code:""}, {name:"Unknown languages", code:"unknown"}];
+
+            $($(".select-menu-list[role='menu']").children()[1]).children().each(function(i, element) {
+                var currentLanguage = {};
+                currentLanguage.name = $(this).text().trim();
+                currentLanguage.code = $(this).attr("href").replace("https://github.com/trending/","");
+                languages.push(currentLanguage);
+            });
+
+            callback(languages);
+        } else if (err && resp.statusCode != 200) {
+            throw "Error: " + err + "\n with status code: " + resp.statusCode;
+        } else {
+            throw "Unknown error"
+        }
+    });
+}
+
+api.trendingRepos = function(callback, languageCode, timePeriod) {
+    languageCode = languageCode || "";
+    timePeriod = timePeriod || "daily"
+    if (!callback) {
+        throw "Callback required";
+    }
+    request("https://github.com/trending" + (languageCode.length > 0 ? "/" : "") + languageCode + "?since=" + timePeriod, function(err, resp, html) {
+        if (!err && resp.statusCode == 200) {
+            var $ = cheerio.load(html);
+
+            var repos = [];
+
+            $(".repo-list").children().each(function(i, element) {
+                var repository = {};
+                repository.author = $(this).find("h3").text().split("/")[0].trim();
+                repository.name = $(this).find("h3").text().split("/")[1].trim();
+                repository.description = $(this).find(".repo-list-description").text().trim();
+                repository.url = "https://github.com/" + repository.author + "/" + repository.name;
+
+                var stats = $(this).find(".repo-list-meta").text().split("•");
+
+                if (stats[0].includes("star")) {
+                    repository.language = null;
+                    repository.stars = stats[0].trim();
+                } else if (!$(this).find(".repo-list-meta").text().includes("star")) {
+                    repository.language = stats[0].trim();
+                    repository.stars = null;
+                } else {
+                    repository.language = stats[0].trim();
+                    repository.stars = stats[1].trim();
+                }
+
+                repos.push(repository);
+            });
+
+            callback(repos);
+        } else if (err && resp.statusCode != 200) {
+            throw "Error: " + err + "\n with status code: " + resp.statusCode;
+        } else {
+            throw "Unknown error"
         }
     });
 }
