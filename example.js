5c3c7f77b21a4ed552d1eed76af9658fc371cda2
Ninjaprawn <george.r.dan@gmail.com>
2016-09-27 13:20:38 +1000

    Polished examples

--- example.js
+++ example.js
@@ -1,28 +1,27 @@
 var exp = require('./index') || require('explore-api');
 
-// exp.topShowcases(function(showcases) {
-// 	console.log(showcases[0]);
-// 	exp.showcaseDetails(showcases[0].url, function(showcase) {
-// 		console.log(showcase);
-// 	});
-// });
+// Get the top showcases
+exp.topShowcases(function(showcases) {
+	// Log the contents of the first showcase
+	console.log(showcases[0]);
+	// Get details of the first showcase
+	exp.showcaseDetails(showcases[0].url, function(showcase) {
+		console.log(showcase);
+	});
+});
 
+// Get the available languages
+exp.availableTrendingLanguages(function(langs) {
+	// Log them
+	console.log(langs);
+});
 
+// Get the trending repositories of the month, for any language
+exp.trendingRepos(function(repos) {
+	console.log(repos);
+}, "", "monthly");
 
-// exp.availableTrendingLanguages(function(langs) {
-// 	console.log(langs);
-// });
-
-// exp.trendingRepos(function(repos) {
-// 	console.log(repos);
-// }, "", "monthly");
-
+// Get 3 showcases from all the showcases, skipping the first showcase
 exp.allShowcases(function(showcases) {
 	console.log(showcases);
-	console.log(showcases.length);
-}, 3);
-
-exp.allShowcases(function(showcases) {
-	console.log(showcases);
-	console.log(showcases.length);
 }, 3, 1);
